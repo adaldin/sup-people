0cd91948e3d47fe34e31e0ed8c638cb2d7785cd3
Hiena <aledaldin@gmail.com>
2022-11-14 15:12:58 +0100

    fixed env variables

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const key = process.env.REACT_APP_FIREBASE_KEY;
+
+export const keys = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+export const mapKey = process.env.REACT_APP_MAP_KEY;
+export const geocodingKey = process.env.REACT_APP_GEOCODING_KEY;
+export const windyMapsKey = process.env.REACT_APP_WINDY_KEY;
+export const openWeatherKey = process.env.REACT_APP_OPENWEATHER_KEY;
+export const mapID = process.env.REACT_APP_MAP_ID;
